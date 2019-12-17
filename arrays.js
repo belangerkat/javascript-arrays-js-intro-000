d1e1f34e9922035fbbb995d00636cdb8fade51e6
Katie Belanger <belangerkat@gmail.com>
2019-12-17 19:52:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray("array", "element") {
   return array.unshift("element")
+}
+function destructivelyAddElementToBeginningOfArray("array", "element") {
+  return ["element", ..."array"]
 }
