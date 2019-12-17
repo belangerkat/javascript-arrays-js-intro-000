601872e7dfcc9840f92fe53695b3953519ba575a
Katie Belanger <belangerkat@gmail.com>
2019-12-17 20:06:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,16 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray("array", "element") {
-  return array.unshift("element");
+  return ["element", ..."array"];
 }
 function destructivelyAddElementToBeginningOfArray("array", "element") {
-  return ["element", ..."array"];
+  array.unshift("element");
+  return array
 }
 function addElementToEndOfArray("array", "element") {
   return ["array", ..."element"];
 }
 function destructivelyAddElementToEndOfArray("array", "element") {
-  return array.push("element");
+  array.push("element");
+  return array
 }
